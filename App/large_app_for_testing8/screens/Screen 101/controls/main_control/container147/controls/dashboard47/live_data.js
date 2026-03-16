92af5ded9f8ccc55b750330dc1ac52aaafa9c695
Mrinal Gupta <tabil31649@eqvox.com>
2026-03-16 10:37:09 +0000

    'large_app_for_testing8' App created

---
+++ App/large_app_for_testing8/screens/Screen 101/controls/main_control/container147/controls/dashboard47/live_data.js
@@ -0,0 +1,5 @@
+{
+    "header_text": "Available Listings ",
+    "tag_text": "12.5%",
+    "title_text": "75 "
+}
---
+++ App/large_app_for_testing8/screens/Screen 23/controls/main_control/tabbedcontainer1/bind_data.js
@@ -0,0 +1,23 @@
+[
+    {
+        "label": "Billing History",
+        "value": "Billing History ",
+        "icon": "icon-home",
+        "active": true,
+        "image": ""
+    },
+    {
+        "label": "Support Tickets",
+        "value": "Support Tickets",
+        "icon": "icon-organization",
+        "active": false,
+        "image": ""
+    },
+    {
+        "label": "Contract",
+        "value": "Contract",
+        "icon": "icon-handbag",
+        "active": false,
+        "image": ""
+    }
+]
